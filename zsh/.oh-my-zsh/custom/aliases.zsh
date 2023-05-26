4d5c04a87b373d455377073fb1bfe6d090a113a9
Simon Lundius <simon.lundius@cyperfection.de>
2023-05-26 08:06:36 +0200

    update aliases

---
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,12 @@
+alias ls="ls --color -lh"
+alias ll="ls --color -alh"
+alias mkdir="mkdir -pv"
+alias ping="ping -c 5"
+alias c="clear"
+alias dotfiles="code -n ~/.dotfiles"
+alias ffs="sudo !!"
+#git
+alias gs="git status"
+#ddev
+alias cf="ddev typo3cms cache:flush"
+alias t3="ddev typo3cms"
