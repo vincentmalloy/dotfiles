913fd4aae60bbbb83dba0a6cf030c54bef2f887d
Simon Lundius <simon.lundius@cyperfection.de>
2023-07-20 16:08:22 +0200

    remove obsolete git alias

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -7,8 +7,6 @@ alias dotfiles="code -n ~/.dotfiles"
 alias ffs="sudo !!"
 alias update="sudo apt update && sudo apt upgrade -y"
 alias e="explorer.exe ." # Open current directory in Windows Explorer
-#git
-alias gs="git status"
 #ddev
 alias cf="ddev typo3cms cache:flush"
 alias t3="ddev typo3cms"
